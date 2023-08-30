05bfd473d2e057e7171af00fe6eceacb79b3a2f0
pitchayatisa <pitchayatisa.h@wisdommoney.co.th>
2023-08-30 10:58:30 +0700

    edit config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
 module.exports = nextConfig
